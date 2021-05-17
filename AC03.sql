2be519990879680c99dbb5ee4236d53182743119
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-05-17 00:05:42 -0300

    Update AC03.sql

--- AC03.sql
+++ AC03.sql
@@ -1,10 +1,10 @@
 --Utilizando a tabela de produtos, selecione as colunas DESCRICAO, COD_PRODUTO, QTD_REAL, QTD_MINIMA 
---calcule a diferença da quantidade (real - mínima) e "apelide" como "diferença de qtd" *
+--calcule a diferenï¿½a da quantidade (real - mï¿½nima) e "apelide" como "diferenÃ§a de qtd" *
 
 SELECT DESCRICAO, COD_PRODUTO, QTD_REAL, QTD_MINIMA, QTD_REAL - QTD_MINIMA AS DIFERENCA_DE_QTD FROM TB_PRODUTO
 
---Você está atuando em um equipe de desenvolvimento e será preciso desenvolver uma consulta sql para 
---exibir um único fornecedor que tenha a maior código de fornecedor, o nome comece com M e que o código país seja maior que 0. *
+--Vocï¿½ estï¿½ atuando em um equipe de desenvolvimento e serï¿½ preciso desenvolver uma consulta sql para 
+--exibir um ï¿½nico fornecedor que tenha a maior cï¿½digo de fornecedor, o nome comece com M e que o cï¿½digo paï¿½s seja maior que 0. *
 
 SELECT TOP 1 COD_FORNECEDOR, NOME, COD_PAIS FROM TB_FORNECEDOR WHERE NOME LIKE 'M%' and COD_PAIS >0 ORDER BY COD_FORNECEDOR DESC
 
@@ -14,9 +14,9 @@ SELECT COD_FORNECEDOR, NOME, COD_PAIS FROM TB_FORNECEDOR ORDER BY COD_FORNECEDOR
 
 SELECT TOP 11 * FROM TB_EMPREGADO ORDER BY CODFUN ASC
 
--- Vinicius, monte um SELECT que retorne todos os pedidos de 2019 que a situação seja entregue ('E'). 
---Ordene primeiro pelo código do vendedor decrescentemente e depois os pedidos do maior para o menor valor. *
+-- Vinicius, monte um SELECT que retorne todos os pedidos de 2019 que a situaï¿½ï¿½o seja entregue ('E'). 
+--Ordene primeiro pelo cï¿½digo do vendedor decrescentemente e depois os pedidos do maior para o menor valor. *
 
 SELECT * FROM TB_PEDIDO
 
-SELECT * FROM TB_PEDIDO WHERE DATA_EMISSAO >= '20190101' and DATA_EMISSAO <= '20191231' AND SITUACAO = 'E' ORDER BY CODVEN, VLR_TOTAL DESC
+SELECT * FROM TB_PEDIDO WHERE DATA_EMISSAO >= '20190101' and DATA_EMISSAO <= '20191231' AND SITUACAO = 'E' ORDER BY CODVEN DESC, VLR_TOTAL DESC
